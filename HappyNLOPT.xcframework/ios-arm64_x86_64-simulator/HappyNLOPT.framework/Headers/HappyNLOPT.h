b72a538f0801c3ece65e518aa6b8cb2e2c33d11c
Haris Ali <syedhali07@gmail.com>
2022-05-25 18:14:14 -0500

    Fix xcframework (doesn't need Foundation)

--- HappyNLOPT.xcframework/ios-arm64_x86_64-simulator/HappyNLOPT.framework/Headers/HappyNLOPT.h
+++ HappyNLOPT.xcframework/ios-arm64_x86_64-simulator/HappyNLOPT.framework/Headers/HappyNLOPT.h
@@ -5,14 +5,9 @@
 //  Created by Haris Ali on 5/25/22.
 //
 
-#import <Foundation/Foundation.h>
+#ifndef HappyNLOPT_h
+#define HappyNLOPT_h
 
-//! Project version number for HappyNLOPT.
-FOUNDATION_EXPORT double HappyNLOPTVersionNumber;
+#include <HappyNLOPT/nlopt.h>
 
-//! Project version string for HappyNLOPT.
-FOUNDATION_EXPORT const unsigned char HappyNLOPTVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <HappyNLOPT/PublicHeader.h>
-
-#import "nlopt.h"
+#endif /* HappyNLOPT_h */
